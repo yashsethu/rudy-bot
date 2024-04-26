878501c33e69e3f8cec514e46c229fa25883992e
Yash Sethuraman <yashsethu@gmail.com>
2024-04-25 20:12:06 -0700

    fixes

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+# Imports
 from discord import Intents
 from discord.ext import commands
 
@@ -7,56 +8,48 @@ intents.typing = False
 intents.presences = False
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+votes = {}
 
-class Speech(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.votes = {}
+speech_time = 0
 
-    @bot.command(name="speech", description="Commands related to the speech")
-    async def speech(self, ctx, subcommand: str, time: int):
-        if subcommand == "vote":
-            if time is None:
-                await ctx.respond("Please set a time.")
-            elif not isinstance(time, int) or not 0 <= time <= 60:
-                await ctx.respond(
-                    "Invalid vote. Please vote for a time between 0 and 60 minutes."
-                )
-            else:
-                user_id = str(ctx.author.id)  # Convert the user ID to a string
-                if user_id in self.votes:  # Check if the user has already voted
-                    await ctx.respond("You have already voted.")
-                else:
-                    self.votes[user_id] = (
-                        time  # Store the vote with the user ID as a string
-                    )
-                    print(self.votes)  # Print out the votes dictionary
-                    await ctx.respond(f"You voted for {time} minutes.")
-        elif subcommand == "set":
-            if time is None:
-                await ctx.respond("Please set a time.")
-            elif not isinstance(time, int) or not 0 <= time <= 60:
-                await ctx.respond(
-                    "Invalid time. Please set a time between 0 and 60 minutes."
-                )
-            else:
-                if not self.votes:  # Check if votes is not empty
-                    await ctx.respond("No votes have been cast yet.")
-                    await ctx.respond(
-                        f"The speech time has been set to {time} minutes."
-                    )
-                    return
-                closest = min(
-                    self.votes.keys(), key=lambda k: abs(self.votes[k] - time)
-                )
-                await ctx.respond(f"The speech time has been set to {time} minutes.")
-                await ctx.respond(
-                    f"The actual time is {time} minutes. The closest vote was {self.votes[closest]} minutes by <@{closest}>."
-                )
+speech = bot.create_group("speech", "Commands related to the speech")
+
+
+@speech.command()
+async def vote(ctx, time: int):
+    if not isinstance(time, int) or not 0 <= time <= 60:
+        await ctx.respond(
+            "Invalid vote. Please vote for a time between 0 and 60 minutes."
+        )
+        return
+    else:
+        user_id = str(ctx.author.id)  # Convert the user ID to a string
+        if user_id in votes:  # Check if the user has already voted
+            await ctx.respond("You have already voted.")
+            return
         else:
-            await ctx.respond("Invalid subcommand.")
+            votes[user_id] = time  # Store the vote with the user ID as a string
+            await ctx.respond(f"You voted for {time} minutes.")
+            return
 
 
-bot.add_cog(Speech(bot))
+@speech.command()
+async def set(ctx, time: int):
+    if not ctx.author.guild_permissions.administrator:
+        await ctx.respond("Sorry, you are not an admin.")
+        return
+    elif not isinstance(time, int) or not 0 <= time <= 60:
+        await ctx.respond("Invalid time. Please set a time between 0 and 60 minutes.")
+        return
+    else:
+        if not votes:  # Check if votes is not empty
+            await ctx.respond("No votes have been cast yet.")
+        closest = min(votes.keys(), key=lambda k: abs(votes[k] - time))
+        await ctx.respond(f"The speech time has been set to {time} minutes.")
+        await ctx.respond(
+            f"The actual time is {time} minutes. The closest vote was {votes[closest]} minutes by <@{closest}>."
+        )
+        votes.clear()
+
 
 bot.run("MTIzMjUxMjQ2MTAyNzAxNjczNQ.GQQJRh.OFixnxYaV65xvFgUVtUVqYsjHK985ooxdgHdys")
