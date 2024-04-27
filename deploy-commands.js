abaa72b2f37217306d12f8ecb1406a548d1f02c4
Yash Sethuraman <yashsethu@gmail.com>
2024-04-27 09:58:24 -0700

    fix random errors

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,8 @@
 const { REST, Routes } = require("discord.js");
 const fs = require("node:fs");
 const path = require("node:path");
+require("dotenv").config();
+console.log(process.env);
 
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
@@ -28,7 +30,7 @@ for (const folder of commandFolders) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(token);
+const rest = new REST().setToken(process.env.DISCORD_BOT_TOKEN);
 
 // and deploy your commands!
 (async () => {
@@ -38,9 +40,12 @@ const rest = new REST().setToken(token);
     );
 
     // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(Routes.applicationCommands(clientId), {
-      body: commands,
-    });
+    const data = await rest.put(
+      Routes.applicationCommands(process.env.clientID),
+      {
+        body: commands,
+      }
+    );
 
     console.log(
       `Successfully reloaded ${data.length} application (/) commands.`
